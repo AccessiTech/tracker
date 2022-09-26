22a8ef24351aaf4eb286ff1ede12588ec18a4a12
AccessiT3ch <accessiT3ch@gmail.com>
2022-09-26 11:56:00 -0700

    Configure store and router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from './store/store';
+import { Provider } from 'react-redux';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+        </Route>
+      </Routes>
+    </HashRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
