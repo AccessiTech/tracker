b0d9fe84f0c30d36e1b4522e8d2ae06d6ce353e3
AccessiT3ch <accessiT3ch@gmail.com>
2022-09-26 12:18:03 -0700

    Include bootstrap styling

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
