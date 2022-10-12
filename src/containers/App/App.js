e74642934ab34fce3ceffa71d53e2edbd60b2bc7
AccessiT3ch <accessiT3ch@gmail.com>
2022-10-12 10:35:30 -0700

    Convert Home list to table

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,9 +3,10 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./App.scss";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { useGetLogsArray } from "../../store/Log";
+import { removeLog, useGetLogsArray } from "../../store/Log";
+import store from "../../store/store";
 
 function App() {
   const navigate = useNavigate();
@@ -18,18 +19,65 @@ function App() {
           <h1>Tracker</h1>
           <h3>Your logs</h3>
 
-          {logs && logs.length ? (
-            <ul>
-              {logs.map((log) => (
-                <li key={log.id}>
-                  <Link to={"/edit/" + log.id}>{log.name}</Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No logs yet.</p>
-          )}
-          
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Log</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {logs && logs.length ? (
+                logs.map((log) => (
+                  <tr key={log.id}>
+                    <td>
+                      <Link to={`/edit/${log.id}`}>{log.name}</Link>
+                    </td>
+                    <td>
+                      <Dropdown
+                        as={ButtonGroup}
+                        id={`table__dropdown_button__${log.id}`}
+                        className="table__dropdown_button"
+                      >
+                        <Button
+                          variant="primary"
+                          onClick={() => navigate(`/log/${log.id}/entry`)}
+                        >
+                          Add Entry
+                        </Button>
+                        <Dropdown.Toggle
+                          split
+                          variant="primary"
+                          id={`table__dropdown_toggle__${log.id}`}
+                          className="table__dropdown_toggle"
+                        />
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item
+                            onClick={() => navigate(`/edit/${log.id}`)}
+                          >
+                            Edit
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                            className="text-danger"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              store.dispatch(removeLog({ logId: log.id}));
+                            }}
+                          >
+                            Delete
+                          </Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <p>No logs yet.</p>
+              )}
+            </tbody>
+          </table>
+
           <Button
             variant="primary"
             onClick={(e) => {
