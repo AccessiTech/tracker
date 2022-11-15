280c960aa6222caf7f243664be80eea9e38b10a7
AccessiT3ch <accessiT3ch@gmail.com>
2022-11-15 15:46:29 -0800

    Add CSV options to set metadata to include

--- src/components/PortDataModal/PortDataModal.tsx
+++ src/components/PortDataModal/PortDataModal.tsx
@@ -1,6 +1,8 @@
 import React, {FC, ReactElement, useEffect} from "react";
+import { Button, Form } from "react-bootstrap";
 import {  Modal } from "react-bootstrap";
 import { Log, useGetLog } from "../../store/Log";
+import { PRIMARY } from "../../strings";
 import { logEntriesToCSV } from "../../utils";
 
 export interface PortDataModalProps {
@@ -15,17 +17,24 @@ export const GENERATING_CSV = "Generating CSV...";
 
 export const PortDataModal : FC<PortDataModalProps> = ({ show, logID, onHide }): ReactElement => {
   const [exportCSV, setExportCSV] = React.useState("");
+  const [includeID, setIncludeID] = React.useState(false);
+  const [includeCreatedAt, setIncludeCreatedAt] = React.useState(false);
+  const [includeUpdatedAt, setIncludeUpdatedAt] = React.useState(false);
   const log:Log = useGetLog(logID);
 
   useEffect(() => {
     if (log) {
-      const csv = logEntriesToCSV(log);
+      const csv = logEntriesToCSV(log, {
+        includeID,
+        includeCreatedAt,
+        includeUpdatedAt,
+      });
       setExportCSV(csv);
     } else {
       setExportCSV("");
       onHide();
     }
-  }, [log]);
+  }, [log, includeID, includeCreatedAt, includeUpdatedAt]);
 
   return (
     <Modal
@@ -38,17 +47,60 @@ export const PortDataModal : FC<PortDataModalProps> = ({ show, logID, onHide }):
       </Modal.Header>
       <Modal.Body>
         {exportCSV.length > 0 && (
-          <a
-            href={"data:text/csv;charset=utf-8," + exportCSV}
-            download={log.name + ".csv"}
+          <Form
           >
-            {DOWNLOAD_CSV}
-          </a>
+            <Form.Group controlId="includeIDCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Include Field ID"
+                checked={includeID}
+                onChange={(e) => {
+                  setExportCSV("");
+                  setIncludeID(e.target.checked);
+                }}
+              />
+              </Form.Group>
+              <Form.Group controlId="includeCreatedAtCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Include Created At"
+                checked={includeCreatedAt}
+                onChange={(e) => {
+                  setExportCSV("");
+                  setIncludeCreatedAt(e.target.checked);
+                }}
+              />
+              </Form.Group>
+              <Form.Group controlId="includeUpdatedAtCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Include Updated At"
+                checked={includeUpdatedAt}
+                onChange={(e) => {
+                  setExportCSV("");
+                  setIncludeUpdatedAt(e.target.checked);
+                }}
+              />
+            </Form.Group>
+          </Form>
         ) || (
           <p>{GENERATING_CSV}</p>
         )}
       </Modal.Body>
-
+      <Modal.Footer>
+        <Button
+          variant={PRIMARY}
+          onClick={(e) => {
+            e.preventDefault();
+            const blob = new Blob([exportCSV], { type: "text/csv" });
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.setAttribute("href", url);
+            link.setAttribute("download", `${log.name}.csv`);
+            link.click();
+          }}
+        >{DOWNLOAD_CSV}</Button>
+      </Modal.Footer>
     </Modal>
   );
 };
--- src/utils.ts
+++ src/utils.ts
@@ -18,13 +18,32 @@ export const slugify = (text: string) => {
   return text && text.toString().toLowerCase().replace(/\s+/g, "-");
 };
 
+export interface logEntriesToCSVOptions {
+  includeID?: boolean;
+  includeCreatedAt?: boolean;
+  includeUpdatedAt?: boolean;
+}
+
 /**
  * Convert Log Entries to CSV
  * @param {Log} logEntries - The log entries to convert
+ * @param {object} options - The options to use
  * @returns {string} The CSV string
  */
-export const logEntriesToCSV = (log: Log) => {
-  let csv = "id,createdAt,updatedAt,";
+export const logEntriesToCSV = (
+  log: Log,
+  options: logEntriesToCSVOptions = {
+    includeID: false,
+    includeCreatedAt: false,
+    includeUpdatedAt: false,
+  }
+) => {
+  const { includeID, includeCreatedAt, includeUpdatedAt } = options;
+  let csv = "";
+  if (includeID) csv += "ID,";
+  if (includeCreatedAt) csv += "Created At,";
+  if (includeUpdatedAt) csv += "Updated At,";
+
   const fields = Object.values(log.fields);
   for (const field of fields) {
     csv += field.name + ",";
@@ -33,7 +52,9 @@ export const logEntriesToCSV = (log: Log) => {
   csv += "\r\n";
   const entries: any[] = Object.values(log.entries);
   for (const entry of entries) {
-    csv += entry.id + "," + entry.createdAt + "," + entry.updatedAt + ",";
+    if (includeID) csv += entry.id + ",";
+    if (includeCreatedAt) csv += entry.createdAt + ",";
+    if (includeUpdatedAt) csv += (entry.updatedAt || "") + ",";
     for (const field of fields) {
       csv += (entry as any).values[field.id] + ",";
     }
