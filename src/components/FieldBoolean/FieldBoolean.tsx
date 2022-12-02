68feed9a3bb8b3d28e4736a4e29047c2ac114c86
AccessiT3ch <accessiT3ch@gmail.com>
2022-12-02 10:43:52 -0800

    Change boolean entry label and switch size

--- src/components/FieldBoolean/FieldBoolean.tsx
+++ src/components/FieldBoolean/FieldBoolean.tsx
@@ -5,12 +5,12 @@ import { BooleanLogField } from "../../store/Log";
 import {
   ASTERISK,
   CHECKBOX,
-  DEFAULT,
   EMPTY,
   SWITCH,
   TEXT_DANGER,
   TEXT_MUTED,
 } from "../../strings";
+import "./fieldBoolean.scss";
 
 export interface FieldBooleanProps
   extends FormikProps<{ [key: string]: string }> {
@@ -19,19 +19,28 @@ export interface FieldBooleanProps
 
 export const FieldBoolean: FC<FieldBooleanProps> = (props): ReactElement => {
   const { values, errors, touched, handleChange, handleBlur } = props;
-  const { id: fieldId, name, required, defaultValue, option } = props.field;
+  const {
+    id: fieldId,
+    name,
+    required,
+    defaultValue,
+    option,
+    trueLabel,
+    falseLabel,
+  } = props.field;
 
-  const fieldLabel = `${name}${required ? ASTERISK : EMPTY}`; 
-  const defaultValueString = `${DEFAULT}${
-    typeof defaultValue === "undefined" ? false : defaultValue
-  }`;
+  const fieldLabel = `${name}${required ? ASTERISK : EMPTY}`;
+  const valueString = "Value: " + (values[fieldId] ? trueLabel : falseLabel);
 
   return (
     <>
       <Form.Group>
         <Form.Label>{fieldLabel}</Form.Label>
+        <br />
         <Form.Check
+          className={`entry__field_boolean__${option}`}
           type={option === SWITCH ? SWITCH : CHECKBOX}
+          inline={option !== SWITCH}
           name={fieldId}
           onChange={handleChange}
           onBlur={handleBlur}
@@ -43,9 +52,7 @@ export const FieldBoolean: FC<FieldBooleanProps> = (props): ReactElement => {
 
         {(touched[fieldId] && errors[fieldId] && (
           <Form.Text className={TEXT_DANGER}>{errors[fieldId]}</Form.Text>
-        )) || (
-          <Form.Text className={TEXT_MUTED}>{defaultValueString}</Form.Text>
-        )}
+        )) || <Form.Text className={TEXT_MUTED}>{valueString}</Form.Text>}
       </Form.Group>
     </>
   );
