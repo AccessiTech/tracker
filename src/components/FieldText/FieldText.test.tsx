bb9a3d07a5d694038e8b658074eedbe7ce8e8fed
AccessiT3ch <accessiT3ch@gmail.com>
2023-02-03 15:07:22 -0800

    Add field text error message tests

--- src/components/FieldText/FieldText.test.tsx
+++ src/components/FieldText/FieldText.test.tsx
@@ -78,3 +78,77 @@ test("renders the field as an input when specified", () => {
   const input = container.querySelector("input[type='text']");
   expect(input).toBeInTheDocument();
 });
+
+test("renders error message when field is required and empty", () => {
+  const thisField = {
+    ...field,
+    required: true,
+  };
+  const thisFormikProps = {
+    ...formikProps,
+    values: { [thisField.id]: "" },
+    touched: { [thisField.id]: true },
+    errors: { [thisField.id]: "Required" },
+  };
+  const { getByText } = render(<FieldText {...thisFormikProps} field={thisField} />);
+  expect(getByText("Required")).toBeInTheDocument();
+});
+
+test("renders error message when field is below minimum length", () => {
+  const thisField = {
+    ...field,
+    minLength: 10,
+  };
+  const thisFormikProps = {
+    ...formikProps,
+    values: { [thisField.id]: "Hello" },
+    touched: { [thisField.id]: true },
+    errors: { [thisField.id]: "Must be at least 10 characters" },
+  };
+  const { getByText } = render(<FieldText {...thisFormikProps} field={thisField} />);
+  expect(getByText("Must be at least 10 characters")).toBeInTheDocument();
+});
+
+test("renders error message when field is above maximum length", () => {
+  const thisField = {
+    ...field,
+    maxLength: 5,
+  };
+  const thisFormikProps = {
+    ...formikProps,
+    values: { [thisField.id]: "Hello World" },
+    touched: { [thisField.id]: true },
+    errors: { [thisField.id]: "Must be at most 5 characters" },
+  };
+  const { getByText } = render(<FieldText {...thisFormikProps} field={thisField} />);
+  expect(getByText("Must be at most 5 characters")).toBeInTheDocument();
+});
+
+test("does not render error message when field is required and not empty", () => {
+  const thisField = {
+    ...field,
+    required: true,
+  };
+  const thisFormikProps = {
+    ...formikProps,
+    values: { [thisField.id]: "Hello World" },
+    touched: { [thisField.id]: true },
+  };
+  const { queryByText } = render(<FieldText {...thisFormikProps} field={thisField} />);
+  expect(queryByText("Required")).not.toBeInTheDocument();
+});
+
+test("does not render error message when field is above minimum length or below maximum length", () => {
+  const thisField = {
+    ...field,
+    minLength: 5,
+    maxLength: 15,
+  };
+  const thisFormikProps = {
+    ...formikProps,
+    values: { [thisField.id]: "Hello World" },
+    touched: { [thisField.id]: true },
+  };
+  const { queryByText } = render(<FieldText {...thisFormikProps} field={thisField} />);
+  expect(queryByText("Must be at least 5 characters")).not.toBeInTheDocument();
+});
